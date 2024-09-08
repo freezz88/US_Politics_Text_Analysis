19cf1306bb63ce98deb406af773f87828589f37c
freezz88 <60326724+freezz88@users.noreply.github.com>
2024-09-08 20:50:48 +0200

    More Structures. Testing version control

--- US_Politics_Text_Analysis.py
+++ US_Politics_Text_Analysis.py
@@ -6,11 +6,49 @@
 
 '''Import der Bibliotheken'''
 
+'''Definition der Klasse und Initialisierung'''
+
+class PoliticsNLP():
+    
+    def __init__(self, datapath, chosenTopics):
+        self.datapath = datapath
+        self.chosenTopics = chosenTopics
+        
 '''Variablendeklaration'''
+
+    politicsNLP = PoliticsNLP("C:\data.csv",0)
 
 '''Eingabe'''
 
+    def inputData():
+        '''ToDo'''
+
+'''Text-Vorverarbeitung'''
+
+    def calculateBoW():
+        '''ToDo'''
+        
+    def calculateTfidf():
+        '''ToDo'''
+        
+'''Validierung'''
+
+    def calculateCoherenceScore():
+        '''ToDo'''
+        
 '''Verarbeitung'''
+        
+    def calculateLSA():
+        '''ToDo'''
+        
+    def calculateLDA():
+        '''ToDo'''
 
 '''Ausgabe'''
+
+    def printNLPdata():
+        '''ToDo'''
+        
+    def plotNLPdata():
+        '''ToDo'''
 
