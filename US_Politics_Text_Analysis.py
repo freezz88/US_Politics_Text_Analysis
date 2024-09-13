ddc3c174cebee0baa0914f4f0cbe0cc0038fe4a7
freezz88 <60326724+freezz88@users.noreply.github.com>
2024-09-13 16:41:59 +0200

    Implementation part 3: cleaning data
    Delete duplicates, empty text and id correction.

--- US_Politics_Text_Analysis.py
+++ US_Politics_Text_Analysis.py
@@ -1,22 +1,32 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[45]:
+# In[49]:
 
 
 '''Import der Bibliotheken'''
 import pandas as pd
 import numpy as np
-import nltk
+
 import gensim
 from pprint import pprint
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import TruncatedSVD
 from sklearn.decomposition import LatentDirichletAllocation
-from nltk.corpus import stopwords
 from gensim.models.coherencemodel import CoherenceModel
 import gensim.corpora as corpora
+# For tokenization - ToDo maybe not necessary with fit_transform
+from nltk.tokenize import word_tokenize
+nltk.download("punkt")
+nltk.download('wordnet')
+# For lemmatization - ToDo maybe not necessary with fit_transform
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet
+nltk.download('averaged_perceptron_tagger')
+# For stopwords
+import nltk
+from nltk.corpus import stopwords
 
 '''Method for reading data from csv and save as type DataFrame (Pandas)'''
 '''index column in this special data set is the third column'''
@@ -30,6 +40,15 @@ data_url = 'https://github.com/freezz88/US_Politics_Text_Analysis/blob/main/redd
 data = inputData(data_url)
 #print(data)
 
+'''Cleaning data'''
+# delete duplicate reviews - column body
+data.drop_duplicates(subset='body', inplace=True)
+# delete reviews without text
+data.dropna(subset=['body'], inplace=True)
+# Reset the index after the deletion of rows
+data.reset_index(drop=True, inplace=True)
+#print(data.head())
+
 '''Filter text for the category comments'''
 '''Only show the column body, the others doesnt matter'''
 data_reviews = data['title'] == "Comment"
@@ -39,6 +58,19 @@ filtered_data_list = data[data_reviews]
 reviews = filtered_data_list['body']
 #reviews_string = str(reviews)
 print(reviews)
+
+'''Text Preprocessing'''
+'''I Tokenization - ToDo maybe not necessary with fit_transform '''
+'''II Download and definition of stopwords with NLTK - ToDo append new stopwords'''
+nltk.download("stopwords")
+stop_words_english = set(stopwords.words('english'))
+'''III Stemming / Lemmatization - ToDo maybe not necessary with fit_transform'''
+
+
+# In[ ]:
+
+
+
 
 
 # In[9]:
