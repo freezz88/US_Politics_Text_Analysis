a9608154869013364ef0fed818daa359f50b66f1
freezz88 <60326724+freezz88@users.noreply.github.com>
2024-09-13 14:12:24 +0200

    Implementation part 1: Input data
    Input data as .csv from github

--- US_Politics_Text_Analysis.py
+++ US_Politics_Text_Analysis.py
@@ -1,54 +1,61 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[11]:
 
 
 '''Import der Bibliotheken'''
+import pandas as pd
 
-'''Definition der Klasse und Initialisierung'''
-
-class PoliticsNLP():
+'''Class Definition'''
+class US_Politics_Text_Analysis:
     
-    def __init__(self, datapath, chosenTopics):
-        self.datapath = datapath
-        self.chosenTopics = chosenTopics
-        
-'''Variablendeklaration'''
+    '''Constructor'''
+    def __init__(self):
+        self.chosenTopics = 0
 
-    politicsNLP = PoliticsNLP("C:\data.csv",0)
+    '''Method for reading data from csv and save as type DataFrame (Pandas)'''
+    def inputData(self, url):
+        input_data_csv = pd.read_csv(url,index_col=0)
+        print(input_data_csv)
+    
+    '''Initialising and declaration'''
+    my_US_Politics_Text_Analysis = US_Politics_Text_Analysis()
+    
+    '''Data Input as .csv from github'''
+    '''@param: ?raw=true in url important for using clean original data'''
+    data_url = 'https://github.com/freezz88/US_Politics_Text_Analysis/blob/main/reddit_politics.csv?raw=true'
+    my_US_Politics_Text_Analysis.inputData(data_url)
 
-'''Eingabe'''
 
-    def inputData():
-        '''ToDo'''
+# In[4]:
 
-'''Text-Vorverarbeitung'''
 
-    def calculateBoW():
-        '''ToDo'''
-        
-    def calculateTfidf():
-        '''ToDo'''
-        
-'''Validierung'''
+'''Implementation in near future'''
+def calculateBoW():
+    print("Method Test")
 
-    def calculateCoherenceScore():
-        '''ToDo'''
-        
-'''Verarbeitung'''
-        
-    def calculateLSA():
-        '''ToDo'''
-        
-    def calculateLDA():
-        '''ToDo'''
+def calculateTfidf():
+    print("Method Test")
 
-'''Ausgabe'''
+def calculateCoherenceScore():
+    print("Method Test")
 
-    def printNLPdata():
-        '''ToDo'''
-        
-    def plotNLPdata():
-        '''ToDo'''
+def calculateLSA():
+    print("Method Test")
+
+def calculateLDA():
+    print("Method Test")
+
+def printNLPdata():
+    print("Method Test")
+
+def plotNLPdata():
+    print("Method Test")
+
+
+# In[ ]:
+
+
+
 
