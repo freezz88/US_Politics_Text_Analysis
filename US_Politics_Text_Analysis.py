5dfad635da7adfa7c7d5eaae01291e1e7b910647
freezz88 <60326724+freezz88@users.noreply.github.com>
2024-09-13 16:27:09 +0200

    Implementation part 2: filter data
    Filter data. Column body. Definition index column.

--- US_Politics_Text_Analysis.py
+++ US_Politics_Text_Analysis.py
@@ -1,34 +1,47 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[11]:
+# In[45]:
 
 
 '''Import der Bibliotheken'''
 import pandas as pd
+import numpy as np
+import nltk
+import gensim
+from pprint import pprint
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import TruncatedSVD
+from sklearn.decomposition import LatentDirichletAllocation
+from nltk.corpus import stopwords
+from gensim.models.coherencemodel import CoherenceModel
+import gensim.corpora as corpora
 
-'''Class Definition'''
-class US_Politics_Text_Analysis:
+'''Method for reading data from csv and save as type DataFrame (Pandas)'''
+'''index column in this special data set is the third column'''
+def inputData(url):
+    input_data_csv = pd.read_csv(url,index_col=2)
+    return input_data_csv
     
-    '''Constructor'''
-    def __init__(self):
-        self.chosenTopics = 0
+'''Data Input as .csv from github'''
+'''@param: ?raw=true in url important for using clean original data'''
+data_url = 'https://github.com/freezz88/US_Politics_Text_Analysis/blob/main/reddit_politics.csv?raw=true'
+data = inputData(data_url)
+#print(data)
 
-    '''Method for reading data from csv and save as type DataFrame (Pandas)'''
-    def inputData(self, url):
-        input_data_csv = pd.read_csv(url,index_col=0)
-        print(input_data_csv)
-    
-    '''Initialising and declaration'''
-    my_US_Politics_Text_Analysis = US_Politics_Text_Analysis()
-    
-    '''Data Input as .csv from github'''
-    '''@param: ?raw=true in url important for using clean original data'''
-    data_url = 'https://github.com/freezz88/US_Politics_Text_Analysis/blob/main/reddit_politics.csv?raw=true'
-    my_US_Politics_Text_Analysis.inputData(data_url)
+'''Filter text for the category comments'''
+'''Only show the column body, the others doesnt matter'''
+data_reviews = data['title'] == "Comment"
+filtered_data_list = data[data_reviews]
+#filtered_dataframe = pd.DataFrame(filtered_data_list)
+#reviews = filtered_dataframe['body']
+reviews = filtered_data_list['body']
+#reviews_string = str(reviews)
+print(reviews)
 
 
-# In[4]:
+# In[9]:
 
 
 '''Implementation in near future'''
