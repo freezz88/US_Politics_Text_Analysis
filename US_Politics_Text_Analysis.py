1b12f9c392b979a1ab02442d30ab9b2cae12f7b9
freezz88 <60326724+freezz88@users.noreply.github.com>
2024-09-17 20:19:51 +0200

    Implementation part 5: BoW, LSA, LDA, Coherence
    Implementing the main funktions for BoW, LSA, LDA and Coherence Score.

--- US_Politics_Text_Analysis.py
+++ US_Politics_Text_Analysis.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[15]:
 
 
-'''Import der Bibliotheken'''
+'''import the needed libraries'''
 import pandas as pd
 import numpy as np
 import os
@@ -20,7 +20,9 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import TruncatedSVD
 from sklearn.decomposition import LatentDirichletAllocation
 from gensim.models.coherencemodel import CoherenceModel
+from gensim.test.utils import common_corpus, common_dictionary
 import gensim.corpora as corpora
+from gensim.models.ldamodel import LdaModel
 # For tokenization - ToDo maybe not necessary
 from nltk.tokenize import word_tokenize
 nltk.download("punkt")
@@ -33,9 +35,12 @@ nltk.download('averaged_perceptron_tagger')
 # For stopwords
 import nltk
 from nltk.corpus import stopwords
+# For transforming SKLearn Coherence in Gensim Coherence
+import tmtoolkit
+from tmtoolkit.topicmod.evaluate import metric_coherence_gensim
 
 
-# In[28]:
+# In[16]:
 
 
 '''Method for reading data from csv and save as type DataFrame (Pandas)'''
@@ -44,8 +49,11 @@ def inputData(url):
     input_data_csv = pd.read_csv(url,index_col=2)
     return input_data_csv
     
+'''Declaration of variables'''
 '''Data Input as .csv from github'''
+'''@model: placeholder, that will be overwritten'''
 '''@param: ?raw=true in url important for using clean original data'''
+model = TruncatedSVD(n_components=10,algorithm='randomized',n_iter=10)
 data_url = 'https://github.com/freezz88/US_Politics_Text_Analysis/blob/main/reddit_politics.csv?raw=true'
 data = inputData(data_url)
 print(data.head(10))
@@ -53,7 +61,7 @@ print(type(data))
 print("Number of rows in DataFrame: ", len(data))
 
 
-# In[29]:
+# In[18]:
 
 
 '''Cleaning data'''
@@ -69,38 +77,21 @@ print("Number of rows in DataFrame after Cleaning: ",len(data))
 '''Only show the column body, the others doesnt matter'''
 data_reviews = data['title'] == "Comment"
 filtered_data_list = data[data_reviews]
-#filtered_dataframe = pd.DataFrame(filtered_data_list)
-#reviews = filtered_dataframe['body']
 reviews = filtered_data_list['body']
-reviews_string = str(reviews)
-print("Sentences after filtering the dataset:")
+# important: index = false removes the indexnumbers. Should not be visible in string representation.
+reviews_string = reviews.to_string(index=False)
+print("Text after filtering the dataset:")
 print(reviews)
 print(type(reviews))
 print(" ")
 
 '''Text Preprocessing'''
 '''I Tokenization - ToDo check later for optimization '''
-# Load the spaCy model
-nlp = spacy.load("en_core_web_sm")
-# Tokenize the text
-doc = nlp(reviews_string)
-# Extract tokens
-tokens = [token.text for token in doc]
-#print(tokens)
+
 '''II Download and definition of stopwords with NLTK - ToDo append new stopwords'''
 nltk.download("stopwords")
 stop_words_english = set(stopwords.words('english'))
 '''III Stemming / Lemmatization - ToDo check later for optimization'''
-
-# For Stemming
-# Initialize the stemmer
-stemmer = PorterStemmer()
-# Stemming each token
-stemmed_tokens = [stemmer.stem(token) for token in tokens]
-print("Sentences after Stemming:")
-print(stemmed_tokens)
-print(type(stemmed_tokens))
-print(" ")
 
 def preprocess_text(text):
     # Convert to lowercase
@@ -113,37 +104,60 @@ def preprocess_text(text):
     text = re.sub(r'(.)\1{2,}', r'\1\1', text)   
     return text
 
+'''Execution of text preprocessing'''
 changed_data = preprocess_text(reviews_string)
-print("Sentences after Text Preprocessing:")
-print(changed_data)
-print(type(changed_data))
-print(" ")
-dict_data = {changed_data.index : changed_data} # ToDo index has to be separate saved from dataframe
-#print(dict_data)
-#print(type(dict_data))
+
+'''Convert string into a list. Split by lines.'''
+list_changed_data = changed_data.splitlines()
+
+# Use the spaCy model
+#nlp = spacy.load("en_core_web_sm")
+# Tokenize the text
+#doc = nlp(list_changed_data)
+# Extract tokens
+#tokens = [token.text for token in doc]
+#print("DataType tokens: ",type(tokens))
+#print(tokens)
 #print(" ")
 
+# For Stemming
+# Initialize the stemmer
+#stemmer = PorterStemmer()
+# Stemming each token
+#stemmed_tokens = [stemmer.stem(token) for token in tokens]
+#print("Sentences after Stemming:")
+#print(stemmed_tokens)
+#print(type(stemmed_tokens))
+#print(" ")
 
-# In[14]:
+# converting list into series datatype
+preprocessed_data = pd.Series(list_changed_data)
+print("Text after text preprocessing:")
+print(preprocessed_data)
+
+
+# In[20]:
 
 
 '''Implementation Bag-of-words'''
-'''ToDo wrong data - the data from text preprocessing not used'''
 def calculateBoW():
     vect = CountVectorizer(stop_words=stop_words_english)
-    #data = vect.fit_transform([reviews_string])
-    bow_data = vect.fit_transform(reviews)
+    bow_data = vect.fit_transform(preprocessed_data)
     bow_data = pd.DataFrame(bow_data.toarray(),columns=vect.get_feature_names())
     '''Zwischenausgabe der Bow-Modell Daten'''
     print("BoW-Modell Daten")
     print(" ")
     print(bow_data)
     print(" ")
+    print("Höchste Wortvorkommen: ")
+    print(bow_data.max())
+    print(" ")
+    
     
 calculateBoW()
 
 
-# In[15]:
+# In[22]:
 
 
 '''Implementation Tf-idf'''
@@ -153,35 +167,78 @@ def calculateTfidf():
     vectorizer = TfidfVectorizer(use_idf=True,
     smooth_idf=True, stop_words=stop_words_english)
     #model = vectorizer.fit_transform([reviews_string]) wrong usage, wrong datatype
-    model = vectorizer.fit_transform(reviews)
+    model = vectorizer.fit_transform(preprocessed_data)
     data2=pd.DataFrame(model.toarray(),columns=vectorizer.get_feature_names())
     '''Zwischenausgabe der TF-idf Daten'''
     print("TF-idf Daten Reviews")
     print(" ")
     print(data2)
     print(" ")
+    print("Höchstes relatives Wortvorkommen: ")
+    print(data2.max())
+    print(" ")
+    return model
     
-calculateTfidf()
+model = calculateTfidf()
 
 
-# In[ ]:
+# In[23]:
 
 
-'''Implementation in near future'''
-def calculateCoherenceScore():
-    print("Method Test")
+def calculateCoherenceScore(model):
+    '''Implementierung des Coherence Score für die Ermittlung der optimalen Anzahl an Topics'''
+    '''Tests jeweils mit 2 bis 8 Topics'''
+    for x in range(2,8):
+        model = LdaModel(common_corpus, num_topics=x) # wrong model only for texting
+        c_model=CoherenceModel(model=model, corpus=common_corpus, dictionary=common_dictionary, coherence='u_mass')
+        coherence = c_model.get_coherence()
+        print("Coherence topic_number=",x," result ",coherence)
 
-def calculateLSA():
-    print("Method Test")
+def calculateLSA(model):
+    '''Implementierung der LSA-Technik der semantischen Analyse'''
+    '''Tests jeweils mit 2 bis 8 Topics'''
+    for x in range(2,8):
+        lsa_model = TruncatedSVD(n_components=x,algorithm='randomized',n_iter=10)
+        lsa = lsa_model.fit_transform(model)
+        l=lsa[0]
+        '''Zwischenausgabe der LSA Daten'''
+        print("Latente semantische Analyse LSA mit Themenanzahl ",x)
+        print(" ")
+        print("Reviews:")
+        for i,topic in enumerate(l):
+            print("Topic ",i," : ", topic)
+        print(" ")
 
-def calculateLDA():
-    print("Method Test")
+def calculateLDA(model):
+    '''Implementierung der LDA-Technik der semantischen Analyse'''
+    '''Tests jeweils mit 2 bis 8 Topics'''
+    lda_model=LatentDirichletAllocation(n_components=2,learning_method='online',random_state=42,max_iter=1)
+    for x in range(2,8):
+        lda_model=LatentDirichletAllocation(n_components=x,learning_method='online',random_state=42,max_iter=1)
+        lda_top=lda_model.fit_transform(model)
+        '''Zwischenausgabe der LDA Daten'''
+        print("Latente Dirichlet Allocation LDA mit Themenanzahl ",x)
+        print(" ")
+        print("Reviews: ")
+        for i,topic in enumerate(lda_top[0]):
+            print("Topic ",i," ID "," : ",topic)
+        print(" ")
+        print(" ")
+        print("Documents by topic matrix: ",lda_top.shape)
+        print("Topic by word matrix: ",lda_model.components_.shape)
+        print(" ")
+    return lda_model
 
 def printNLPdata():
     print("Method Test")
 
+
 def plotNLPdata():
     print("Method Test")
+    
+calculateLSA(model)
+model = calculateLDA(model)
+calculateCoherenceScore(model)
 
 
 # In[ ]:
